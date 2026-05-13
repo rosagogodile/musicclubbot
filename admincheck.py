586ca543d189f84660f0e9328754f31fe33cb342
rosagogodile <luciagogodile@gmail.com>
2026-05-12 23:51:37 -0400

    started actually writing commands

---
+++ admincheck.py
@@ -0,0 +1,7 @@
+"""
+Written by Rosa Marie Knowles 
+Initial Commit: 05/12/2026 
+Latest Commit:  05/12/2026
+
+Code to check that the user submitting a command is an admin.
+"""
--- main.py
+++ main.py
@@ -17,5 +17,22 @@ if __name__ == "__main__":
     # get token
     load_dotenv()
 
-    token = str(os.getenv("TOKEN"))
-    print(token)
+    TOKEN = str(os.getenv("TOKEN"))
+
+
+    # intialize bot 
+    bot = discord.Bot()
+
+    # prints when the bot is online 
+    # useful for debugging?
+    @bot.event
+    async def on_ready():
+        print(f"{bot.user} is online.")
+
+    @bot.slash_command(name="test", description="testing testing 123")
+    async def test(ctx: discord.ApplicationContext):
+        await ctx.respond("test command!")
+        await ctx.respond(str(ctx.user))
+
+    # run the bot 
+    bot.run(TOKEN)
